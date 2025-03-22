bee6b7fc62d5b73bec1c5745b547c60e2f172e97
Riyasingh139 <singhriya99568@gmail.com>
2025-03-22 18:12:54 +0530

    Fixed CORS issue and updated API

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ app.use("/api/blogs",blogRoutes)
 app.use("/api/services",serviceRoutes)
 
 
-// Serve static files from React build
-app.use(express.static(path.join(__dirname, "client", "build")));
+// // Serve static files from React build
+// app.use(express.static(path.join(__dirname, "client", "build")));
 
-// Catch-all route to serve `index.html`
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// // Catch-all route to serve `index.html`
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 // Start Server
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
